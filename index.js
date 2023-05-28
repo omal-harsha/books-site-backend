0d22ff67c92e4f456873c1b3080eaa71e16c8b32
omal-harsha <omalharshanuwan@gmail.com>
2023-05-28 10:19:20 +0530

    initial files
    install necessary packages and connect db

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import {db} from "./database.js"
+
+const app = express()
+
+app.get("/", (req,res) => {
+    res.json("hello this is the backend")
+})
+
+app.get("/books", (req,res) => {
+    const q = "SELECT * FROM books";
+    db.query(q, (err,data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    }) 
+})
+
+app.listen(8800, () => {
+    console.log("Connected to backend")
+})
