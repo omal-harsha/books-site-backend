32f2ad51ed5f0395f387b1513ddc10283cd34970
omal-harsha <omalharshanuwan@gmail.com>
2023-05-28 13:02:59 +0530

    add bodyparser

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2"
 
-/*export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "abc123",
-    database: "library"
-})*/
+// export const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "abc123",
+//     database: "library"
+// })
 
 export const db = mysql.createConnection({
     host: "us-cdbr-east-06.cleardb.net",
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import {db} from "./database.js"
 
 const app = express()
+app.use(express.json());
 
 app.get("/", (req,res) => {
     res.json("hello this is the backend")
@@ -16,14 +17,14 @@ app.get("/books", (req,res) => {
 })
 
 app.post("/createbooks", (req,res) => {
-    const q =  "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?,?,?)"
+    const q =  "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
     const values = [
         req.body.title,
         req.body.desc,
         req.body.cover,
     ]
 
-    db.query(q,values, (err,data) => {
+    db.query(q,[values], (err,data) => {
         if(err) return res.json(err)
         return res.json("book has been created successfully")
     })
