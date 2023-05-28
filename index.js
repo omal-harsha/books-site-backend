936f81bee36e1f3d6b9233a3646a8d8b65e7e936
omal-harsha <omalharshanuwan@gmail.com>
2023-05-28 10:40:27 +0530

    troubelshoot heroku
    comment the code lines

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {db} from "./database.js"
+//import {db} from "./database.js"
 
 const app = express()
 
@@ -7,13 +7,13 @@ app.get("/", (req,res) => {
     res.json("hello this is the backend")
 })
 
-app.get("/books", (req,res) => {
-    const q = "SELECT * FROM books";
-    db.query(q, (err,data) => {
-        if(err) return res.json(err)
-        return res.json(data)
-    }) 
-})
+// app.get("/books", (req,res) => {
+//     const q = "SELECT * FROM books";
+//     db.query(q, (err,data) => {
+//         if(err) return res.json(err)
+//         return res.json(data)
+//     }) 
+// })
 
 app.listen(8800, () => {
     console.log("Connected to backend")
