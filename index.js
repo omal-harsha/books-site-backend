cacd27d2b9f29b4e5218bfd918a8d11f31185514
omal-harsha <omalharshanuwan@gmail.com>
2023-05-31 11:50:12 +0530

    create all CRUD operation

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import {db} from "./database.js"
+import cors from 'cors'
 
 const app = express()
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req,res) => {
     res.json("hello this is the backend")
@@ -16,12 +18,51 @@ app.get("/books", (req,res) => {
     }) 
 })
 
+app.get("/getbook/:id",(req,res) => {
+    const bookId =  req.params.id
+    const q =  "SELECT * FROM books WHERE id = ?"
+
+    db.query(q,bookId, (err,data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.delete("/books/:id", (req,res) => {
+    const bookId =  req.params.id
+    const q = "DELETE FROM books WHERE id = ?"
+
+    db.query(q,bookId, (err,data) =>{
+        if(err) return res.json(err)
+        return res.json("Book has been delete successfully")
+    })
+})
+
+app.put("/update/:id", (req,res) => {
+
+    const bookId = req.params.id
+    const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ? " 
+
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.price,
+        req.body.cover,
+    ]
+
+    db.query(q, [...values, bookId], (err,data) => {
+        if(err) return res.json(err)
+        return res.json("book has been updated")
+    })
+})
+
 app.post("/createbooks", (req,res) => {
-    const q =  "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
+    const q =  "INSERT INTO books (`title`, `desc`, `cover`,`price`) VALUES (?)"
     const values = [
         req.body.title,
         req.body.desc,
         req.body.cover,
+        req.body.price,
     ]
 
     db.query(q,[values], (err,data) => {
