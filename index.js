99814c3220699fb46e14dc31da2f281670411198
omal-harsha <omalharshanuwan@gmail.com>
2023-05-31 14:49:53 +0530

    set cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express"
+import cors from 'cors';
 import {db} from "./database.js"
 
 const app = express()
 app.use(express.json());
+app.use(cors()); 
 
 app.get("/", (req,res) => {
     res.json("hello this is the backend")
