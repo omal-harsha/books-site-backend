1c7cfc44fffc10ccf70e7a6b124477649278aa8f
Omal Harsha <69068196+omal-harsha@users.noreply.github.com>
2023-05-31 13:16:29 +0530

    Update index.js
    added getbook, books/:id, update/:id routes

--- index.js
+++ index.js
@@ -8,6 +8,44 @@ app.get("/", (req,res) => {
     res.json("hello this is the backend")
 })
 
+app.get("/getbook/:id",(req,res) => {
+    const bookId =  req.params.id
+    const q =  "SELECT * FROM books WHERE id = ?"
+
+    db.query(q,bookId, (err,data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.delete("/books/:id", (req,res) => {
+    const bookId =  req.params.id
+    const q = "DELETE FROM books WHERE id = ?"
+
+    db.query(q,bookId, (err,data) =>{
+        if(err) return res.json(err)
+        return res.json("Book has been delete successfully")
+    })
+})
+
+app.put("/update/:id", (req,res) => {
+
+    const bookId = req.params.id
+    const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ? " 
+
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.price,
+        req.body.cover,
+    ]
+
+    db.query(q, [...values, bookId], (err,data) => {
+        if(err) return res.json(err)
+        return res.json("book has been updated")
+    })
+})
+
 app.get("/books", (req,res) => {
     const q = "SELECT * FROM books";
     db.query(q, (err,data) => {
